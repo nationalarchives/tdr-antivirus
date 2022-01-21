97166d6d724b985b10f373a347cbbb06bef9d225
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-01-21 13:57:37 +0000

    Find a better way to check if there are updates needed.

--- run_av_tests.py
+++ run_av_tests.py
@@ -3,12 +3,17 @@ import os
 
 import sys
 rules = yara.load("/rules/output")
-match_len = 0
-for file in os.listdir("/testfiles"):
-    matches = rules.match(f"/testfiles/{file}")
-    av_message = "No match found" if len(matches) == 0 else " ".join([m.rule for m in matches])
-    print(f"{file} {av_message}")
-    match_len = match_len + len(matches)
+rules_old = yara.load("/rules/output-old")
 
-if match_len > 0:
-    sys.exit(1)
+match_len = 0
+if sorted([ro.identifier for ro in rules_old]) == sorted([r.identifier for r in rules]):
+    for file in os.listdir("/testfiles"):
+        matches = rules.match(f"/testfiles/{file}")
+        av_message = "No match found" if len(matches) == 0 else " ".join([m.rule for m in matches])
+        print(f"{file} {av_message}")
+        match_len = match_len + len(matches)
+
+    if match_len > 0:
+        sys.exit(1)
+else:
+    sys.exit(3)
