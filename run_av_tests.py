e2410f4bfff5690c636e2b7de6370dde9e950fcd
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-01-21 14:16:04 +0000

    Replace equals with not equals.

--- run_av_tests.py
+++ run_av_tests.py
@@ -6,7 +6,7 @@ rules = yara.load("/rules/output")
 rules_old = yara.load("/rules/output-old")
 
 match_len = 0
-if sorted([ro.identifier for ro in rules_old]) == sorted([r.identifier for r in rules]):
+if sorted([ro.identifier for ro in rules_old]) != sorted([r.identifier for r in rules]):
     for file in os.listdir("/testfiles"):
         matches = rules.match(f"/testfiles/{file}")
         av_message = "No match found" if len(matches) == 0 else " ".join([m.rule for m in matches])
