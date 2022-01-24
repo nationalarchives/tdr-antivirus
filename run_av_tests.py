e49af73fba198970e6a921756824f2037d68190f
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-01-24 09:45:54 +0000

    Use proper logging and show which rules have changed.

--- run_av_tests.py
+++ run_av_tests.py
@@ -1,16 +1,25 @@
 import yara
 import os
+import logging
 
 import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
 rules = yara.load("/rules/output")
 rules_old = yara.load("/rules/output-old")
 
 match_len = 0
-if sorted([ro.identifier for ro in rules_old]) != sorted([r.identifier for r in rules]):
+rules_old_id = sorted([ro.identifier for ro in rules_old])
+rules_id = sorted([r.identifier for r in rules])
+
+if rules_old_id != rules_id:
+    changed_rules = [item for item in rules_old_id if item not in rules_id]
+    logging.debug(f"Rules have changed. The following rules are different {changed_rules}")
     for file in os.listdir("/testfiles"):
         matches = rules.match(f"/testfiles/{file}")
         av_message = "No match found" if len(matches) == 0 else " ".join([m.rule for m in matches])
-        print(f"{file} {av_message}")
+        logging.debug(f"{file} {av_message}")
         match_len = match_len + len(matches)
 
     if match_len > 0:
