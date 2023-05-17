aa0271d20b9943f8cfffca2e6a74876cdc65a759
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-05-17 16:56:23 +0100

    More dependency updates

--- build-dependencies.sh
+++ build-dependencies.sh
@@ -6,7 +6,7 @@
 yum update -y
 yum install -y autoconf automake bzip2-devel gcc.x86_64 gcc-c++.x86_64 libarchive-devel libffi-devel \
         libtool libuuid-devel openssl-devel pcre-devel poppler-utils python3-pip python3-devel zlib-devel \
-            wget make gcc-c++ xz libpng-devel
+            wget make gcc-c++ xz libpng-devel python3-setuptools
 
 # Compile YARA
 YARA_VERSION=$1
@@ -23,22 +23,20 @@ make install
 cd /
 pip3 install --upgrade pip
 mkdir pip
-/usr/local/bin/pip3 install --requirement requirements.txt --target pip
+pip3 install --requirement requirements.txt --target pip
 
 # Clean cryptography files
 cd /pip
 rm -r *.dist-info *.egg-info
 find . -name __pycache__ | xargs rm -r
-mv _cffi_backend.cpython-37m-x86_64-linux-gnu.so _cffi_backend.so
+mv _cffi_backend.cpython-39-x86_64-linux-gnu.so _cffi_backend.so
 cd cryptography/hazmat/bindings
-mv _openssl.abi3.so _openssl.so
-mv _padding.abi3.so _padding.so
 
 # Gather pip files
 cd /
 mkdir lambda
 cp -r pip/* lambda
-mv lambda/yara.cpython-37m-x86_64-linux-gnu.so lambda/yara.so
+mv lambda/yara.cpython-39-x86_64-linux-gnu.so lambda/yara.so
 
 # Download UPX
 cd /
@@ -52,15 +50,15 @@ cp /usr/bin/pdftotext lambda
 cp /usr/lib64/libarchive.so.13 lambda
 cp /usr/lib64/libfontconfig.so.1 lambda
 cp /usr/lib64/libfreetype.so.6 lambda
-cp /usr/lib64/libjbig.so.2.0 lambda
+cp /usr/lib64/libjbig.so.2.1 lambda
 cp /usr/lib64/libjpeg.so.62 lambda
 cp /usr/lib64/liblcms2.so.2 lambda
 cp /usr/lib64/liblzma.so.5 lambda
-cp /usr/lib64/liblzo2.so.2 lambda
-cp /usr/lib64/libopenjpeg.so.1 lambda
+cp /usr/lib64/liblz4.so.1 lambda
+cp /usr/lib64/libopenjp2.so.7 lambda
 cp /usr/lib64/libpcrecpp.so.0 lambda
-cp /usr/lib64/libpng15.so lambda
-cp /usr/lib64/libpoppler.so.46 lambda
+cp /usr/lib64/libpng16.so lambda
+cp /usr/lib64/libpoppler.so.123 lambda
 cp /usr/lib64/libstdc++.so.6 lambda
 cp /usr/lib64/libtiff.so.5 lambda
 cp /usr/lib64/libxml2.so.2 lambda
